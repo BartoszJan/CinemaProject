2729d9df4d0e9759689154ad223dc8c9cd7def0d
Bartek <b.janczak90@gmail.com>
2017-09-24 15:33:31 +0200

    +

---
+++ src/main/java/com/mojafirma/MovieApp.java
@@ -0,0 +1,20 @@
+package com.mojafirma;
+
+import com.mojafirma.model.Movie;
+import com.mojafirma.model.dao.MovieDao;
+
+import java.sql.Date;
+
+public class MovieApp {
+
+    public static void main(String[] args) {
+        MovieDao movieDao = new MovieDao();
+        Movie matrix = new Movie();
+        matrix.setTitle("Matrix");
+        matrix.setDuration(120);
+        matrix.setYear(Date.valueOf("1990-01-01"));
+        matrix.setDirector("Wachowsky brothers");
+
+        movieDao.addMovie(matrix);
+    }
+}
--- src/main/java/com/mojafirma/model/dao/MovieDao.java
+++ src/main/java/com/mojafirma/model/dao/MovieDao.java
@@ -1,4 +1,28 @@
 package com.mojafirma.model.dao;
 
+import com.mojafirma.HibernateUtil;
+import com.mojafirma.model.Movie;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class MovieDao {
+
+    public Integer addMovie(Movie movie) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        Integer movieID = null;
+        try {
+            tx = session.beginTransaction();
+            movieID = (Integer) session.save(movie);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return movieID;
+    }
 }
