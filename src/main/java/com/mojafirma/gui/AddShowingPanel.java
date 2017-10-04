78b7cf5822b4e02ae8b6e9e0e874b0d5862d18c7
Bartosz Jańczak <b.janczak90@gmail.com>
2017-10-04 21:47:14 +0200

    +

--- src/main/java/com/mojafirma/gui/AddShowingPanel.java
+++ src/main/java/com/mojafirma/gui/AddShowingPanel.java
@@ -1,11 +1,17 @@
 package com.mojafirma.gui;
 
+import com.mojafirma.model.Movie;
 import com.mojafirma.model.dao.MovieDao;
 import com.mojafirma.util.HibernateUtil;
 import com.mojafirma.model.Showing;
 import com.mojafirma.model.dao.ShowingDao;
+import com.mojafirma.util.MovieComboBoxModel;
+import com.mojafirma.util.MovieListModel;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.event.ListDataListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
@@ -22,6 +28,7 @@ public class AddShowingPanel extends JFrame{
     private JButton button1;
     private JTextField textField1;
     private JTextField movieTitleInput;
+    private JComboBox chooseMovieComboBox;
 
     public AddShowingPanel() {
 
@@ -29,16 +36,9 @@ public class AddShowingPanel extends JFrame{
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+        MovieDao movieDao = new MovieDao();
 
-        panel1.add(headTextField);
-        panel1.add(textField1);
-        panel1.add(movieTitleInput);
-        panel1.add(textField2);
-        panel1.add(dateTimeInput);
-        panel1.add(textField3);
-        panel1.add(roomNumberInput);
-        panel1.add(button1);
+        chooseMovieComboBox.setModel(new MovieComboBoxModel(movieDao.getMovieList()));
 
         button1.addActionListener(new ActionListener() {
             @Override
